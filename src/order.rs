d4481cf6a5199a575f7d416c953417fe37824ebb
substack <substack@bits.coop>
2019-02-27 14:27:48 -1000

    extra constrait for branch factor

--- src/order.rs
+++ src/order.rs
@@ -1,4 +1,9 @@
 pub fn pivot_order (bf: usize) -> Vec<usize> {
+  assert_eq!(
+    (1 << (31 - (bf as u32).leading_zeros()))+1,
+    bf,
+    "branch factor must be a power of 2 plus 1"
+  );
   let n = bf*2-3;
   let mut order = Vec::with_capacity(n);
   for i in 0..((((n+1) as f32).log2()) as usize) {
