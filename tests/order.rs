33f7136bdb9d1e21aa2875cf26e7bf0e16a84313
substack <substack@bits.coop>
2019-02-27 14:22:45 -1000

    fix order tests for correct branch factor consideration

--- tests/order.rs
+++ tests/order.rs
@@ -6,13 +6,13 @@ use order::pivot_order;
 #[test]
 fn order () {
   assert_eq!(
-    pivot_order(4),
+    pivot_order(5),
     vec![3,1,5,0,2,4,6],
-    "order for branch factor 4"
+    "order for branch factor 5"
   );
   assert_eq!(
-    pivot_order(8),
+    pivot_order(9),
     vec![7,3,11,1,5,9,13,0,2,4,6,8,10,12,14],
-    "order for branch factor 8"
+    "order for branch factor 9"
   );
 }
