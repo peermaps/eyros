30d3a6aa397040542eae748cc4c1b5a18b4ec3b6
substack <substack@bits.coop>
2019-04-01 22:17:06 -1000

    remove assert_eq (which calls panic) from ensure_eq

--- src/ensure.rs
+++ src/ensure.rs
@@ -11,16 +11,13 @@ macro_rules! ensure_eq {
       }
     }
   });
-  ($left:expr, $right:expr,) => ({
-    assert_eq!($left, $right)
-  });
-  ($left:expr, $right:expr, $($arg:tt)+) => ({
+  ($left:expr, $right:expr, $($arg:tt)*) => ({
     match (&($left), &($right)) {
       (left_val, right_val) => {
         if !(*left_val == *right_val) {
           return Err(format_err!(r#"assertion failed: `(left == right)`
   left: `{:?}`,
- right: `{:?}`: {}"#, left_val, right_val, format_args!($($arg)+)));
+ right: `{:?}`: {}"#, left_val, right_val, format_args!($($arg)*)));
         }
       }
     }
@@ -40,16 +37,13 @@ macro_rules! ensure_eq_some {
       }
     }
   });
-  ($left:expr, $right:expr,) => ({
-    assert_eq!($left, $right)
-  });
-  ($left:expr, $right:expr, $($arg:tt)+) => ({
+  ($left:expr, $right:expr, $($arg:tt)*) => ({
     match (&($left), &($right)) {
       (left_val, right_val) => {
         if !(*left_val == *right_val) {
           return Some(Err(format_err!(r#"assertion failed: `(left == right)`
   left: `{:?}`,
- right: `{:?}`: {}"#, left_val, right_val, format_args!($($arg)+))));
+ right: `{:?}`: {}"#, left_val, right_val, format_args!($($arg)*))));
         }
       }
     }
