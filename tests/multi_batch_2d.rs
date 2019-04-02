566cb251550eff04a170e92a9371f1e949d70a21
substack <substack@bits.coop>
2019-04-01 22:35:56 -1000

    failing test with mixed types and intervals

--- tests/multi_batch_2d.rs
+++ tests/multi_batch_2d.rs
@@ -12,11 +12,10 @@ use tempfile::Builder as Tmpfile;
 
 use std::cmp::Ordering;
 
-type P = (f32,f64);
-type V = u16;
-
 #[test]
 fn multi_batch_f32_f64_u16() -> Result<(),Error> {
+  type P = (f32,f64);
+  type V = u16;
   let dir = Tmpfile::new().prefix("eyros").tempdir()?;
   let mut db: DB<_,_,P,V> = DB::open(
     |name: &str| -> Result<RandomAccessDisk,Error> {
@@ -74,6 +73,79 @@ fn multi_batch_f32_f64_u16() -> Result<(),Error> {
       })
       .filter(|r| {
         contains_pt((bbox.0).0,(bbox.1).0, (r.0).0)
+        && contains_pt((bbox.0).1,(bbox.1).1, (r.0).1)
+      })
+      .collect();
+    results.sort_unstable_by(cmp);
+    expected.sort_unstable_by(cmp);
+    assert_eq!(results.len(), expected.len(),
+      "incorrect length for partial region");
+    assert_eq!(results, expected, "incorrect results for partial region");
+  }
+  Ok(())
+}
+
+#[test]
+fn multi_batch_f64iv_f32_u16() -> Result<(),Error> {
+  type P = ((f64,f64),f32);
+  type V = u16;
+  let dir = Tmpfile::new().prefix("eyros").tempdir()?;
+  let mut db: DB<_,_,P,V> = DB::open(
+    |name: &str| -> Result<RandomAccessDisk,Error> {
+      let p = dir.path().join(name);
+      Ok(RandomAccessDisk::open(p)?)
+    }
+  )?;
+  let mut r = rand().seed([13,12]);
+  let size = 10_000;
+  let inserts: Vec<Row<P,V>> = (0..size).map(|_| {
+    let x0: f64 = (r.read::<f64>()*2.0-1.0)*1000.0;
+    let x1: f64 = x0 + ((r.read::<f64>().powf(64.0))*2.0-1.0)*500.0;
+    let y: f32 = r.read::<f32>()*4000.0+2000.0;
+    let value: u16 = r.read();
+    Row::Insert(((x0,x1),y), value)
+  }).collect();
+  let n = 5;
+  let batches: Vec<Vec<Row<P,V>>> = (0..n).map(|i| {
+    inserts[size/n*i..size/n*(i+1)].to_vec()
+  }).collect();
+  for batch in batches {
+    db.batch(&batch)?;
+  }
+
+  {
+    let bbox = ((-1500.0,2000.0),(1500.0,6000.0));
+    let mut results = vec![];
+    for result in db.query(&bbox)? {
+      results.push(result?);
+    }
+    assert_eq!(results.len(), size, "incorrect length for full region");
+    let mut expected: Vec<(P,V)> = inserts.iter().map(|r| {
+      match r {
+        Row::Insert(point,value) => (*point,*value),
+        _ => panic!["unexpected row type"]
+      }
+    }).collect();
+    results.sort_unstable_by(cmp);
+    expected.sort_unstable_by(cmp);
+    assert_eq!(results, expected, "incorrect results for full region");
+  }
+
+  {
+    let bbox = ((-800.0,1000.0),(-400.0,4000.0));
+    let mut results = vec![];
+    for result in db.query(&bbox)? {
+      results.push(result?);
+    }
+    let mut expected: Vec<(P,V)> = inserts.iter()
+      .map(|r| {
+        match r {
+          Row::Insert(point,value) => (*point,*value),
+          _ => panic!["unexpected row type"]
+        }
+      })
+      .filter(|r| {
+        contains_iv((bbox.0).0,(bbox.1).0, (r.0).0)
         && contains_pt((bbox.0).1,(bbox.1).1, (r.0).1)
       })
       .collect();
