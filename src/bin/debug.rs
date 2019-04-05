da9f6da4c47bf02f11423f90d6f01f261534071f
substack <substack@bits.coop>
2019-04-04 23:10:25 -1000

    print out all branches in debug command

--- src/bin/debug.rs
+++ src/bin/debug.rs
@@ -51,53 +51,13 @@ fn main() -> Result<(),Error> {
     let i = args[3].parse::<usize>()?;
     let j = args[4].parse::<u64>()?;
     let depth = args[5].parse::<usize>()?;
-    let len = db.trees[i].store.len()? as u64;
-    let buf = read_block(&mut db.trees[i].store, j, len, 1024)?;
-    let bf = 9;
-    let n = bf*2-3;
-
-    let psize = P::pivot_size_at(depth % P::dim());
-    let p_start = 0;
-    let d_start = p_start + n*psize;
-    let i_start = d_start + (n+bf+7)/8;
-    let b_start = i_start + n*size_of::<u64>();
-    let b_end = b_start+bf*size_of::<u64>();
-    assert_eq!(b_end, buf.len(), "unexpected block length");
-
-    let mut fpivots = vec![];
-    for i in 0..n {
-      let pbuf = &buf[p_start+i*psize..p_start+(i+1)*psize];
-      fpivots.push(P::format_at(pbuf, depth)?);
-    }
-    let intersecting: Vec<(bool,u64)> = (0..n).map(|i| {
-      let is_data = ((buf[d_start+i/8]>>(i%8))&1) == 1;
-      let i_offset = i_start + i*8;
-      let offset = u64::from_be_bytes([
-        buf[i_offset+0], buf[i_offset+1],
-        buf[i_offset+2], buf[i_offset+3],
-        buf[i_offset+4], buf[i_offset+5],
-        buf[i_offset+6], buf[i_offset+7],
-      ]);
-      (is_data,offset)
-    }).collect();
-    let buckets: Vec<(bool,u64)> = (0..bf).map(|i| {
-      let j = i+n;
-      let is_data = ((buf[d_start+j/8]>>(j%8))&1) == 1;
-      let b_offset = b_start + i*8;
-      let offset = u64::from_be_bytes([
-        buf[b_offset+0], buf[b_offset+1],
-        buf[b_offset+2], buf[b_offset+3],
-        buf[b_offset+4], buf[b_offset+5],
-        buf[b_offset+6], buf[b_offset+7],
-      ]);
-      (is_data,offset)
-    }).collect();
+    let b = read_branch(&mut db, i, j, depth)?;
     println!("# pivots");
-    for (i,p) in fpivots.iter().enumerate() {
+    for (i,p) in b.str_pivots.iter().enumerate() {
       println!("[{}] {}", i, p);
     }
     println!["# intersecting"];
-    for (i,(is_data,offset)) in intersecting.iter().enumerate() {
+    for (i,(is_data,offset)) in b.intersecting.iter().enumerate() {
       if *offset == 0 {
         println!["[{}] NULL", i];
       } else {
@@ -108,7 +68,7 @@ fn main() -> Result<(),Error> {
       }
     }
     println!["# buckets"];
-    for (i,(is_data,offset)) in buckets.iter().enumerate() {
+    for (i,(is_data,offset)) in b.buckets.iter().enumerate() {
       if *offset == 0 {
         println!["[{}] NULL", i];
       } else {
@@ -144,8 +104,79 @@ fn main() -> Result<(),Error> {
     }
     let elapsed = start.elapsed().as_float_secs();
     println!["{} results in {} seconds", results.len(), elapsed];
+  } else if args[2] == "branches" {
+    let i = args[3].parse::<usize>()?;
+    let mut queue = vec![(0,0)];
+    while !queue.is_empty() {
+      let (offset,depth) = queue.pop().unwrap();
+      println!["{} {}", depth, offset];
+      let b = read_branch(&mut db, i, offset, depth)?;
+      for (is_data,i_offset) in b.intersecting {
+        if !is_data && i_offset > 0 {
+          queue.push((i_offset-1,depth+1))
+        }
+      }
+      for (is_data,b_offset) in b.buckets {
+        if !is_data && b_offset > 0 {
+          queue.push((b_offset-1,depth+1))
+        }
+      }
+    }
   } else {
     bail!["COMMAND not recognized"];
   }
   Ok(())
+}
+
+pub struct Branch {
+  pub str_pivots: Vec<String>,
+  pub intersecting: Vec<(bool,u64)>,
+  pub buckets: Vec<(bool,u64)>
+}
+
+fn read_branch<S,U> (db: &mut DB<S,U,P,V>, tree_i: usize,
+offset: u64, depth: usize) -> Result<Branch,Error>
+where S: RandomAccess<Error=Error>, U: (Fn(&str) -> Result<S,Error>) {
+  let len = db.trees[tree_i].store.len()? as u64;
+  let buf = read_block(&mut db.trees[tree_i].store, offset, len, 1024)?;
+  let bf = 9;
+  let n = bf*2-3;
+
+  let psize = P::pivot_size_at(depth % P::dim());
+  let p_start = 0;
+  let d_start = p_start + n*psize;
+  let i_start = d_start + (n+bf+7)/8;
+  let b_start = i_start + n*size_of::<u64>();
+  let b_end = b_start+bf*size_of::<u64>();
+  assert_eq!(b_end, buf.len(), "unexpected block length");
+
+  let mut str_pivots = vec![];
+  for i in 0..n {
+    let pbuf = &buf[p_start+i*psize..p_start+(i+1)*psize];
+    str_pivots.push(P::format_at(pbuf, depth)?);
+  }
+  let intersecting: Vec<(bool,u64)> = (0..n).map(|i| {
+    let is_data = ((buf[d_start+i/8]>>(i%8))&1) == 1;
+    let i_offset = i_start + i*8;
+    let offset = u64::from_be_bytes([
+      buf[i_offset+0], buf[i_offset+1],
+      buf[i_offset+2], buf[i_offset+3],
+      buf[i_offset+4], buf[i_offset+5],
+      buf[i_offset+6], buf[i_offset+7],
+    ]);
+    (is_data,offset)
+  }).collect();
+  let buckets: Vec<(bool,u64)> = (0..bf).map(|i| {
+    let j = i+n;
+    let is_data = ((buf[d_start+j/8]>>(j%8))&1) == 1;
+    let b_offset = b_start + i*8;
+    let offset = u64::from_be_bytes([
+      buf[b_offset+0], buf[b_offset+1],
+      buf[b_offset+2], buf[b_offset+3],
+      buf[b_offset+4], buf[b_offset+5],
+      buf[b_offset+6], buf[b_offset+7],
+    ]);
+    (is_data,offset)
+  }).collect();
+  Ok(Branch { str_pivots, buckets, intersecting })
 }
