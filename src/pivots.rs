f6ae782d275ea29f4d5de5a3b9bde95b3595497b
substack <substack@bits.coop>
2019-04-10 11:22:15 -1000

    fix stack overflow crash with padding pivots when len==1

--- src/pivots.rs
+++ src/pivots.rs
@@ -2,8 +2,18 @@ use point::Point;
 
 pub fn pad<P> (xs: &Vec<P>, n: usize) -> Vec<P> where P: Point {
   let mut len = xs.len();
-  let slots = (n-len).min(len-1);
+  if len == 0 {
+    panic!["attempted to pad pivots from an empty source"];
+  }
   let mut res = xs.clone();
+  if len == 1 {
+    let r = res[0];
+    for _ in 1..n {
+      res.push(r.clone());
+    }
+    return res
+  }
+  let slots = (n-len).min(len-1);
   for i in 0..slots {
     let k = slots-i-1;
     let j = k*len/slots;
