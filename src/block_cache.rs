3158854e691e0277834ada271cc694004eda2849
substack <substack@bits.coop>
2019-04-19 04:49:26 -1000

    block cache working for full write tests

--- src/block_cache.rs
+++ src/block_cache.rs
@@ -1,42 +1,71 @@
 use random_access_storage::RandomAccess;
 use std::io::Write;
 use lru::LruCache;
+use std::collections::HashMap;
 
+#[derive(Debug,Clone)]
 struct Block {
   pub data: Vec<u8>,
-  pub mask: Vec<u8>
+  pub mask: Vec<u8>,
+  pub missing: usize
 }
 
 impl Block {
   pub fn new (size: usize) -> Self {
     let n = (size+7)/8;
-    Self { data: vec![0;size], mask: vec![0;n] }
+    Self {
+      data: vec![0;size],
+      mask: vec![0;n],
+      missing: size
+    }
   }
   pub fn from_data (data: Vec<u8>) -> Self {
     let n = (data.len()+7)/8;
-    Self { data, mask: vec![0;n] }
+    Self {
+      data,
+      mask: vec![0;n],
+      missing: 0
+    }
   }
   pub fn write (&mut self, offset: usize, data: &[u8]) -> () {
-    eprintln!["WRITE {} {}", offset, data.len()];
     self.data[offset..offset+data.len()].copy_from_slice(data);
     for i in offset..offset+data.len() {
+      let m = (self.mask[i/8] >> (i%8)) & 1 == 1;
+      if !m && self.missing > 0 { self.missing -= 1 }
       self.mask[i/8] |= 1<<(i%8);
     }
   }
+  pub fn merge (&mut self, data: &[u8]) -> () {
+    for i in 0..data.len() {
+      let m = (self.mask[i/8] >> (i%8)) & 1 == 1;
+      if !m {
+        self.data[i] = data[i];
+        self.missing -= 1;
+      }
+    }
+  }
+  pub fn have_all (&self, i: usize, j: usize) -> bool {
+    if self.missing == 0 { return true }
+    for k in i..j {
+      if (self.mask[k/8] >> (k%8)) & 1 == 0 { return false }
+    }
+    true
+  }
+  /*
   pub fn commit<S> (&mut self, store: &mut S, offset: u64)
   -> Result<(),S::Error> where S: RandomAccess {
     store.write(offset as usize, &self.data)?;
     Ok(())
   }
+  */
 }
 
 //#[derive(Debug,Clone)]
 pub struct BlockCache<S> where S: RandomAccess {
   store: S,
   size: usize,
-  count: usize,
-  cache: LruCache<u64,Block>,
-  write_queue: Vec<u64>
+  reads: LruCache<u64,Block>,
+  writes: HashMap<u64,Block>
 }
 
 impl<S> BlockCache<S> where S: RandomAccess {
@@ -44,9 +73,8 @@ impl<S> BlockCache<S> where S: RandomAccess {
     Self {
       store,
       size,
-      count,
-      cache: LruCache::new(count),
-      write_queue: vec![]
+      reads: LruCache::new(count),
+      writes: HashMap::new()
     }
   }
 }
@@ -57,26 +85,35 @@ impl<S> RandomAccess for BlockCache<S> where S: RandomAccess {
     let start = (offset/self.size) as u64;
     let end = ((offset+data.len()+self.size-1)/self.size) as u64;
     let mut d_start = 0;
-    for (j,i) in (start..end).enumerate() {
+    for i in start..end {
       let b = i * (self.size as u64);
       let b_start = ((offset as u64).max(b)-b) as usize;
       let b_len = (((offset+data.len()) as u64 - b) as usize)
-        .min(self.size - b_start);
+        .min(self.size - b_start)
+        .min(data.len());
       let b_end = b_start + b_len;
       let d_end = d_start + b_len;
       let slice = &data[d_start..d_end];
       d_start += b_len;
-      let none = match self.cache.get_mut(&b) {
+      let check_read = match self.writes.get_mut(&b) {
         Some(block) => {
           block.write(b_start, slice);
           false
         },
-        None => { true }
+        None => true
       };
-      if none {
-        let mut block = Block::new(self.size);
-        block.write(((offset as u64).max(b)-b) as usize, slice);
-        self.cache.put(b, block);
+      if check_read {
+        match self.reads.pop(&b) {
+          Some(mut block) => {
+            block.data[b_start..b_end].copy_from_slice(slice);
+            self.writes.insert(b, block);
+          },
+          None => {
+            let mut block = Block::new(self.size);
+            block.write(b_start, slice);
+            self.writes.insert(b, block);
+          }
+        }
       }
     }
     Ok(())
@@ -85,29 +122,85 @@ impl<S> RandomAccess for BlockCache<S> where S: RandomAccess {
   Result<Vec<u8>,Self::Error> {
     let start = (offset/self.size) as u64;
     let end = ((offset+length+self.size-1)/self.size) as u64;
-    let mut result: Vec<u8> = vec![];
+    let mut result: Vec<u8> = vec![0;length];
+    let mut result_i = 0;
+    let mut reads: Vec<(u64,(usize,usize),bool)> = vec![];
     for i in start..end {
       let b = i * (self.size as u64);
       let b_start = ((offset as u64).max(b)-b) as usize;
       let b_len = (((offset+length) as u64 - b) as usize)
-        .min(self.size - b_start);
+        .min(self.size - b_start)
+        .min(length);
       let b_end = b_start + b_len;
-      let none = match self.cache.get(&b) {
+      let range = (result_i, result_i + b_len);
+      result_i += b_len;
+      match self.writes.get(&b) {
         Some(block) => {
-          // TODO: check write mask
-          eprintln!["block.data={:?}", block.data];
-          result.extend_from_slice(&block.data[b_start..b_end]);
-          false
+          if block.have_all(b_start, b_end) {
+            let slice = &block.data[b_start..b_end];
+            result[range.0..range.1].copy_from_slice(slice);
+          } else {
+            reads.push((b,range,true));
+          }
         },
-        None => { true }
+        None => {
+          match self.reads.get(&b) {
+            Some(rblock) => {
+              let slice = &rblock.data[b_start..b_end];
+              result[range.0..range.1].copy_from_slice(slice);
+            },
+            None => { reads.push((b,range,false)) }
+          }
+        }
       };
-      if none {
-        let data = self.store.read(b as usize, length)?;
-        result.extend_from_slice(&data[b_start..b_end]);
-        let block = Block::from_data(data);
-        self.cache.put(b, block);
+    }
+    if !reads.is_empty() {
+      let len = self.store.len()? as u64;
+      let i = reads[0].0.min(len);
+      let j = (reads.last().unwrap().0 + (self.size as u64))
+        .min(self.store.len()? as u64);
+      let data = if j > i {
+        self.store.read(i as usize, (j-i) as usize)?
+      } else { vec![] };
+
+      let len = data.len();
+      for (b,range,write) in reads {
+        let d_start = ((b-i) as usize).min(len);
+        let d_end = (d_start + self.size).min(len);
+        let slice = &data[d_start..d_end];
+
+        let b_start = ((offset as u64).max(b)-b) as usize;
+        let b_len = (((offset+length) as u64 - b) as usize)
+          .min(self.size - b_start)
+          .min(length);
+        let b_end = b_start + b_len;
+
+        if write {
+          match self.writes.get_mut(&b) {
+            Some(block) => {
+              block.merge(&slice);
+              let bslice = &block.data[b_start..b_end];
+              result[range.0..range.1].copy_from_slice(bslice);
+            },
+            None => {
+              panic!["expected block in write cache at offset {}", b]
+            }
+          }
+        } else {
+          let mut vdata = slice.to_vec();
+          if slice.len() < self.size {
+            vdata.extend(vec![0;self.size-slice.len()]);
+          }
+          let block = Block::from_data(vdata);
+          {
+            let bslice = &block.data[b_start..b_end];
+            result[range.0..range.1].copy_from_slice(bslice);
+          }
+          self.reads.put(b, block);
+        }
       }
     }
+    assert_eq![result.len(), length, "correct result length"];
     Ok(result)
   }
   fn read_to_writer (&mut self, _offset: usize, _length: usize,
--- tests/block_cache.rs
+++ tests/block_cache.rs
@@ -3,6 +3,7 @@ extern crate random_access_disk;
 extern crate tempfile;
 extern crate failure;
 extern crate lru;
+extern crate random;
 
 #[path="../src/block_cache.rs"]
 mod block_cache;
@@ -12,16 +13,50 @@ use random_access_disk::RandomAccessDisk;
 use random_access_storage::RandomAccess;
 use tempfile::Builder as Tmpfile;
 use failure::Error;
+use random::{Source, default as rand};
 
 #[test]
-fn block_cache () -> Result<(),Error> {
+fn block_cache_full_write () -> Result<(),Error> {
+  let mut r = rand().seed([13,12]);
   let dir = Tmpfile::new().prefix("eyros-block-cache").tempdir()?;
-  let mut store = BlockCache::new(
-    RandomAccessDisk::open(dir.path().join("0"))?, 10, 20);
-  store.write(0, &vec![97,98,5])?;
-  store.write(2, &vec![99,100,101,102,103,104,105,106,107,108,109])?;
-  assert_eq![store.read(8, 4)?, vec![105,106,107,108]];
-  assert_eq![store.read(10, 3)?, vec![107,108,109]];
-  assert_eq![store.read(0, 4)?, vec![97,98,99,100]];
+  {
+    let mut store = BlockCache::new(
+      RandomAccessDisk::open(dir.path().join("0"))?, 10, 20);
+    store.write(0, &vec![97,98,5])?;
+    store.write(2, &vec![99,100,101,102,103,104,105,106,107,108,109])?;
+    assert_eq![store.read(8, 4)?, vec![105,106,107,108]];
+    assert_eq![store.read(10, 3)?, vec![107,108,109]];
+    assert_eq![store.read(0, 4)?, vec![97,98,99,100]];
+  }
+  {
+    for i in 1..10 {
+      let mut store = BlockCache::new(
+        RandomAccessDisk::open(dir.path().join(i.to_string()))?, 50, 40);
+      let size = 1_000;
+      let mut data = vec![];
+      for _ in 0..size {
+        data.push(r.read::<u8>());
+      }
+      let mut mask = vec![false;size];
+      let mut covered = 0;
+      while covered < size {
+        let i = (r.read::<f64>()*(size as f64)) as usize;
+        let j = i + ((r.read::<f64>()*((size-i+1) as f64)) as usize);
+        store.write(i,&data[i..j])?;
+        for k in i..j {
+          if !mask[k] { covered += 1 }
+          mask[k] = true;
+        }
+      }
+      for _ in 0..500 {
+        let i = (r.read::<f64>()*(size as f64)) as usize;
+        let len = (r.read::<f64>()*((size-i) as f64)) as usize;
+        assert_eq![store.read(i,len)?, data[i..i+len].to_vec()];
+      }
+      assert_eq![store.read(8, 4)?, data[8..8+4].to_vec()];
+      assert_eq![store.read(10, 3)?, data[10..10+3].to_vec()];
+      assert_eq![store.read(0, 4)?, data[0..4].to_vec()];
+    }
+  }
   Ok(())
 }
