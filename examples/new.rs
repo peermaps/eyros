465b7166fb66f3e677da0d9f32b173ec31d8ff8a
substack <substack@bits.coop>
2020-12-04 18:53:15 -1000

    remote old bits of new example

--- examples/new.rs
+++ examples/new.rs
@@ -1,7 +1,5 @@
 use eyros::{DB,Tree2,Row,Coord};
-//use eyros::Tree2;
 use rand::random;
-//use desert::FromBytes;
 use async_std::prelude::*;
 
 type P = (Coord<f32>,Coord<f32>);
@@ -19,16 +17,10 @@ async fn main() -> Result<(),E> {
     let ymin = (random::<f32>()*2.0-1.0)*90.0;
     let ymax = ymin + random::<f32>().powf(16.0)*(90.0-ymin);
     let point = (Coord::Interval(xmin,xmax), Coord::Interval(ymin,ymax));
-    //let value = (random::<f32>() * 10_000.0) as u64;
     Row::Insert(point, i)
   }).collect();
   db.batch(&rows).await?;
 
-  //let bytes = db.trees[0].lock().await.to_bytes()?;
-  //eprintln!["tree={:#?}", <Tree2<f32,f32,V>>::from_bytes(&bytes)?];
-  //eprintln!["{} bytes", bytes.len()];
-
-  //let bbox = ((-50.0,-45.0),(-25.0,-15.0));
   let bbox = ((-180.0,-90.0),(180.0,90.0));
   let mut stream = db.query(&bbox).await?;
   while let Some(result) = stream.next().await {
