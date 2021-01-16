c4372eca8f01d2cc0117472b04ab2f8f494296cf
substack <substack@bits.coop>
2021-01-15 16:10:42 -1000

    some io concurrency

--- src/tree_file.rs
+++ src/tree_file.rs
@@ -3,12 +3,13 @@ use crate::{Tree,TreeId,Error,Point,Value,Storage,RA};
 use std::collections::{HashMap,HashSet};
 use random_access_storage::RandomAccess;
 use async_std::{sync::{Arc,Mutex},task::spawn};
+use async_std::prelude::*;
 
 pub struct TreeFile<S,T,P,V> where T: Tree<P,V>, P: Point, V: Value, S: RA {
   cache: LRU<TreeId,Arc<Mutex<T>>>,
   storage: Arc<Mutex<Box<dyn Storage<S>>>>,
-  updated: Arc<Mutex<HashMap<TreeId,Arc<Mutex<T>>>>>,
-  removed: Arc<Mutex<HashSet<TreeId>>>,
+  updated: HashMap<TreeId,Arc<Mutex<T>>>,
+  removed: HashSet<TreeId>,
   _marker: std::marker::PhantomData<(P,V)>,
 }
 
@@ -17,13 +18,13 @@ impl<S,T,P,V> TreeFile<S,T,P,V> where T: Tree<P,V>, P: Point, V: Value, S: RA {
     Self {
       cache: LRU::new(n),
       storage,
-      updated: Arc::new(Mutex::new(HashMap::new())),
-      removed: Arc::new(Mutex::new(HashSet::new())),
+      updated: HashMap::new(),
+      removed: HashSet::new(),
       _marker: std::marker::PhantomData,
     }
   }
   pub async fn get(&mut self, id: &TreeId) -> Result<Arc<Mutex<T>>,Error> {
-    if let Some(t) = self.updated.lock().await.get(id) {
+    if let Some(t) = self.updated.get(id) {
       return Ok(Arc::clone(t));
     }
     match &self.cache.get(id) {
@@ -41,44 +42,62 @@ impl<S,T,P,V> TreeFile<S,T,P,V> where T: Tree<P,V>, P: Point, V: Value, S: RA {
   pub async fn put(&mut self, id: &TreeId, t: Arc<Mutex<T>>) -> () {
     //eprintln!["put {}", id];
     self.cache.put(*id, Arc::clone(&t));
-    self.updated.lock().await.insert(*id, Arc::clone(&t));
+    self.updated.insert(*id, Arc::clone(&t));
   }
   pub async fn remove(&mut self, id: &TreeId) -> () {
     //eprintln!["remove {}", id];
     self.cache.pop(id);
-    self.updated.lock().await.remove(id);
-    self.removed.lock().await.insert(*id);
+    self.updated.remove(id);
+    self.removed.insert(*id);
   }
   pub async fn flush(&mut self) -> Result<(),Error> {
-    //eprintln!["flush {}", self.updated.len()];
-    {
-      let updated = self.updated.clone();
-      let removed = self.removed.clone();
-      let mut tasks = vec![];
-      let updated_x = updated.lock().await;
-      for (id,t) in updated_x.iter() {
-        let file = get_file(id);
-        let storage = Arc::clone(&self.storage);
-        tasks.push(spawn(async move {
-          let mut s = storage.lock().await.open(&file).await?;
-          s.write(0, &t.lock().await.to_bytes()?).await
-        }));
-      }
-      for id in removed.lock().await.iter() {
-        let file = get_file(id);
-        let storage = self.storage.clone();
-        tasks.push(spawn(async move {
-          // ignoring errors for now
-          storage.lock().await.remove(&file).await
-        }));
-      }
-      // not concurrent, just demonstrating the issue
-      for t in tasks.iter_mut() {
-        t.await?;
-      }
+    let mut tasks = vec![];
+    for (id,t) in self.updated.iter() {
+      let file = get_file(id);
+      let tree = Arc::clone(t);
+      let storage = Arc::clone(&self.storage);
+      tasks.push(spawn(async move {
+        let mut s = storage.lock().await.open(&file).await?;
+        s.write(0, &tree.lock().await.to_bytes()?).await
+      }));
     }
-    self.updated.lock().await.clear();
-    self.removed.lock().await.clear();
+    for id in self.removed.iter() {
+      let file = get_file(id);
+      let storage = self.storage.clone();
+      tasks.push(spawn(async move {
+        // ignoring errors for now
+        storage.lock().await.remove(&file).await;
+        Ok(())
+      }));
+    }
+    let mut itasks = tasks.iter_mut();
+    loop {
+      let a = itasks.next();
+      if a.is_none() { break }
+      let b = itasks.next();
+      if b.is_none() {
+        a.unwrap().await?;
+        break;
+      }
+      let c = itasks.next();
+      if c.is_none() {
+        a.unwrap().try_join(b.unwrap()).await?;
+        break;
+      }
+      let d = itasks.next();
+      if d.is_none() {
+        a.unwrap().try_join(b.unwrap()).try_join(c.unwrap()).await?;
+        break;
+      }
+      a.unwrap()
+        .try_join(b.unwrap())
+        .try_join(
+          c.unwrap().try_join(d.unwrap())
+        )
+        .await?;
+    }
+    self.updated.clear();
+    self.removed.clear();
     Ok(())
   }
 }
